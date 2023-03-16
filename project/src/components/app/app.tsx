ef38621b4f0158ec9e83b7f2dc884c1089778062
Andrew Samohvalov <andrew.samohv@gmail.com>
2023-03-16 13:42:22 +0300

    Добавит маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,38 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+
 import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScren from '../../pages/login-screen/login-screen';
+import RoomScreen from '../../pages/room-screen/room-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppProps = {
   roomCardCount: number;
 };
 
-function App({roomCardCount}: AppProps): JSX.Element {
-  return <MainScreen roomCardCount={roomCardCount}/>;
-}
 
-export default App;
+export default function App({roomCardCount}: AppProps): JSX.Element {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen roomCardCount={roomCardCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScren />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<RoomScreen />}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
