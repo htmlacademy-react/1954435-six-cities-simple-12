dea537b699a6a33bd0d1b1b7a4ca72afe675e3c4
Andrew Samohvalov <andrew.samohv@gmail.com>
2023-06-15 13:51:25 +0300

    Добавляет событие  открытие/закрытие Sorting

--- project/src/components/sorting/sorting.tsx
+++ project/src/components/sorting/sorting.tsx
@@ -1,27 +1,43 @@
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { sortOffer } from '../../store/actions';
 import { SORTS } from '../../const';
 
 export default function Sorting() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const sortType = useAppSelector((state) => state.sortType);
   const dispatch = useAppDispatch();
-
 
   return (
     <form className="places__sorting" action="todo" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={() => setIsOpen(!isOpen)}
+      >
         {sortType}
+        {/* Не отображается стрелка, переделать*/}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="todoicon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul
+        className={`places__options places__options--custom ${
+          isOpen ? 'places__options--opened' : ''
+        }`}
+      >
         {SORTS.map((sort) => {
           const isActive = sort === sortType;
 
           return (
-            <li key={sort} className={`places__option ${ isActive ? 'places__option--active' : ''} `} tabIndex={0}
+            <li
+              key={sort}
+              className={`places__option ${
+                isActive ? 'places__option--active' : ''
+              } `}
+              tabIndex={0}
               onClick={(evt) => {
                 dispatch(sortOffer(sort));
               }}
@@ -29,25 +45,7 @@ export default function Sorting() {
               {sort}
             </li>
           );
-
         })}
-
-        {/*         <li
-          className="places__option places__option--active"
-          tabIndex={0}
-        >
-          Popular
-        </li>
-        <li className="places__option" tabIndex={0}>
-          Price: low to high
-        </li>
-        <li className="places__option" tabIndex={0}>
-          Price: high to low
-        </li>
-        <li className="places__option" tabIndex={0}>
-          Top rated first
-        </li>*/}
-
       </ul>
     </form>
   );
