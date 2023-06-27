1bd642742e1c53ca6b22d1cf1171f551fde30c12
Andrew Samohvalov <andrew.samohv@gmail.com>
2023-06-27 18:44:33 +0300

    Создаст компонент для отображения ошибки и подключит его.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {store} from './store';
 import {Provider} from 'react-redux';
+import ErrorMessage from './components/error-message/error-message';
 
 
 const root = ReactDOM.createRoot(
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ErrorMessage />
       <App />
     </Provider>
   </React.StrictMode>,
