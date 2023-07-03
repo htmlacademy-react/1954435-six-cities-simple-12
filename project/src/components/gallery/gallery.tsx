c43bcde52d620d25c781dcb60e70f980a5ced74f
Andrew Samohvalov <andrew.samohv@gmail.com>
2023-07-03 16:35:53 +0300

    Рефакторит Gallery

--- project/src/components/gallery/gallery.tsx
+++ project/src/components/gallery/gallery.tsx
@@ -6,11 +6,13 @@ type GalleryProps = {
 
 export default function Gallery ({offer}:GalleryProps) {
   const {title, images} = offer;
+  const photos = images.slice(0,6);
+
   return (
     <div className="property__gallery-container container">
       <div className="property__gallery">
 
-        { images.length && images.map((src)=>(
+        { photos.length && photos.map((src)=>(
           <div key={src} className="property__image-wrapper">
             <img className="property__image" src={src} alt={title}/>
           </div>))}
