d968a251992590049ea45e13335578d8d868498d
Andrew Samohvalov <andrew.samohv@gmail.com>
2023-07-17 16:42:45 +0300

    Выводит компоненты в зависимости от статуса авторизации пользователя

--- project/src/components/header-nav/header-nav.tsx
+++ project/src/components/header-nav/header-nav.tsx
@@ -1,18 +1,25 @@
+import { AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks';
+
 import HeaderProfile from '../header-profile/header-profile';
 import HeaderSignOut from '../header-sign-out/header-sign-out';
-//import HeaderSignIn from '../header-sign-in/header-sign-in';
-
+import HeaderSignIn from '../header-sign-in/header-sign-in';
 
 export default function HeaderNav() {
+  const authorizationStatus = useAppSelector((state) => state.offers.authorizationStatus);
+  const isUserLogged = authorizationStatus === AuthorizationStatus.Authorized;
 
   return (
     <nav className="header__nav">
       <ul className="header__nav-list">
-
-        <HeaderProfile />
-
-        <HeaderSignOut/>
-
+        {
+          isUserLogged ?
+            <>
+              <HeaderProfile />
+              <HeaderSignOut />
+            </>
+            : <HeaderSignIn />
+        }
       </ul>
     </nav>
   );
