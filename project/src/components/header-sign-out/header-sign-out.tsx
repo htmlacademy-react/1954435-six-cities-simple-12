93d00452bd92d77eac45c48d0ef0acbf86529200
Andrew Samohvalov <andrew.samohv@gmail.com>
2023-12-12 16:52:45 +0300

    Реализует logout

--- project/src/components/header-sign-out/header-sign-out.tsx
+++ project/src/components/header-sign-out/header-sign-out.tsx
@@ -1,8 +1,18 @@
+import { logoutAction } from '../../store/api-actions';
+import { useAppDispatch } from '../../hooks';
+import { MouseEvent } from 'react';
+
 export default function HeaderSignOut() {
+  const dispatch = useAppDispatch();
+
+  const handleSignOutClick = (evt: MouseEvent<HTMLAnchorElement>) => {
+    evt.preventDefault();
+    dispatch(logoutAction());
+  };
 
   return (
     <li className="header__nav-item">
-      <a className="header__nav-link" href="todo">
+      <a className="header__nav-link" href="todo" onClick={handleSignOutClick}>
         <span className="header__signout">Sign out</span>
       </a>
     </li>
