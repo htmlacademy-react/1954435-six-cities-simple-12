fe89a50880b9a8c53c52c5a56f49a4fe532a493a
Andrew Samohvalov <andrew.samohv@gmail.com>
2024-02-02 18:54:14 +0300

    фиксит форму коментариев

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -18,13 +18,11 @@ export default function ReviewForm() {
   const postReviewstStatus = useAppSelector(getPostReviewStatus);
 
   const isRatingEmpty = rating === null;
-  const isReviewTooShort = review.length < MIN_TEXT_LENGTH;
-  const isReviewTooLong = review.length > MAX_TEXT_LENGHT;
+  const isReviewValid = review.length < MIN_TEXT_LENGTH || review.length > MAX_TEXT_LENGHT;
 
   const isFormDisabled =
-    isReviewTooShort ||
-    isReviewTooLong ||
     isRatingEmpty ||
+    isReviewValid ||
     postReviewstStatus.isLoading;
 
   useEffect(() => {
