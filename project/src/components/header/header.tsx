8a401d4e4391cbe77706a245ab7c0a82a8bb0bfc
Andrew Samohvalov <andrew.samohv@gmail.com>
2024-02-08 22:05:42 +0300

    Добавит компонент Logo и тест

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,5 +1,4 @@
-import {Link} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import Logo from '../logo/logo';
 import HeaderNav from '../header-nav/header-nav';
 
 type HeaderProps = {
@@ -11,17 +10,9 @@ export default function Header(props: HeaderProps) {
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
-          <div className="header__left">
-            <Link className="header__logo-link header__logo-link--active" to={AppRoute.Main}>
-              <img
-                className="header__logo"
-                src="img/logo.svg"
-                alt="6 cities logo"
-                width="81"
-                height="41"
-              />
-            </Link>
-          </div>
+
+          <Logo />
+
           {props.hasNavigation && <HeaderNav />}
 
         </div>
