aa7cdfeded54c0daa3905bfba82e721f3e9d576d
Andrew Samohvalov <andrew.samohv@gmail.com>
2024-02-09 19:10:01 +0300

    Рефактрит тест Error-screen

--- project/src/pages/error-screen/error-screen.test.tsx
+++ project/src/pages/error-screen/error-screen.test.tsx
@@ -2,10 +2,13 @@ import { render, screen } from '@testing-library/react';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import userEvent from '@testing-library/user-event';
 import ErrorScreen from './error-screen';
 import HistoryRouter from '../../components/history-route/history-route';
 
-const mockStore = configureMockStore();
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('Component: ErrorScreen', () => {
   it('should render correctly', () => {
@@ -23,5 +26,21 @@ describe('Component: ErrorScreen', () => {
 
     expect(screen.getByText(/Failed to load data/i)).toBeInTheDocument();
     expect(screen.getByText(/Try again/i)).toBeInTheDocument();
+    expect(screen.getByRole('button').textContent).toBe('Try again');
+  });
+
+  it('should fetch offers if the button is clicked', async () => {
+    const store = mockStore({});
+    render(
+      <Provider store={store}>
+        <ErrorScreen />
+      </Provider>
+    );
+
+    await userEvent.click(screen.getByRole('button'));
+
+    const actions = store.getActions();
+
+    expect(actions[0].type).toBe('data/fetchOffers/pending');
   });
 });
